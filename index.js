431cf287d913983cbda40ed70e4a16d81d9c5375
soso <>
2021-06-12 23:24:09 +0800

    primary function

---
+++ index.js
@@ -0,0 +1,96 @@
+const electron = require('electron')
+const path = require('path')
+
+const { app, BrowserWindow, ipcMain, Tray, Menu, screen } = electron
+const iconPath = path.join(__dirname, './src/img/icon.png')
+
+let mainWindow
+let tray
+let remindWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    frame: false,
+    resizable: false,
+    width: 800,
+    height: 600,
+    icon: iconPath,
+    webPreferences:{
+      backgroundThrottling: false,
+      nodeIntegration:true,
+      contextIsolation: false,
+      preload: path.join(__dirname, './preload.js')
+    }
+  })
+  mainWindow.loadURL(`file://${__dirname}/src/main.html`)
+  
+  tray = new Tray(iconPath)
+  tray.setToolTip('Tasky')
+  tray.on('click', () => {
+    if(mainWindow.isVisible()){
+      mainWindow.hide()
+    }else{
+      mainWindow.show()
+    }
+  })
+  tray.on('right-click', () => {
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ])
+    tray.popUpContextMenu(menuConfig)
+  })
+
+})
+
+ipcMain.on('mainWindow:close', () => {
+  mainWindow.hide()
+})
+
+ipcMain.on('remindWindow:close', () => {
+  remindWindow.close()
+})
+
+ipcMain.on('setTaskTimer', (event, time, task) => {
+  const now = new Date()
+  const date = new Date()
+  date.setHours(time.slice(0,2), time.slice(3),0)
+  const timeout = date.getTime() - now.getTime()
+  setTimeout(() => {
+    createRemindWindow(task)
+  }, timeout)
+})
+
+function createRemindWindow (task) {
+  if(remindWindow) remindWindow.close()
+  remindWindow = new BrowserWindow({
+    height: 450,
+    width: 360,
+    resizable: false,
+    frame: false,
+    icon: iconPath,
+    webPreferences:{
+      nodeIntegration:true,
+      contextIsolation: false,
+      preload: path.join(__dirname, './preload.js')
+    }
+  })
+  const size = screen.getPrimaryDisplay().workAreaSize
+  const { y } = tray.getBounds()
+  const { height, width } = remindWindow.getBounds()
+  const yPosition = process.platform === 'darwin' ? y : y - height
+  remindWindow.setBounds({
+    x: size.width - width,
+    y: yPosition,
+    height,
+    width 
+  })
+  remindWindow.loadURL(`file://${__dirname}/src/remind.html`)
+  remindWindow.webContents.send('setTask', task)
+  remindWindow.on('closed', () => { remindWindow = null })
+  setTimeout( () => {
+    remindWindow && remindWindow.close()
+  }, 50 * 1000)
+}
---
+++ src/js/main.js
@@ -0,0 +1,147 @@
+(function(){
+  const electron = require('electron')
+  const { ipcRenderer } = electron
+
+  const tabManage = document.querySelectorAll('.tab-manage')
+  const contentManage = document.querySelectorAll('.content-manage')
+  const taskTodo = document.querySelector('.task-todo')
+  const taskFinished = document.querySelector('.task-finished')
+  const keepTimesDom = document.querySelector('.keep-times')
+  const closeDom = document.querySelector('.close')
+  const date = new Date(), nowTime = date.getTime()
+  
+  //填入日期
+  document.querySelector('.date').innerText = 
+    `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`
+    /** task data structure
+   * name,
+   * time
+   **/
+
+  //localStorage存储数据
+  let loginTime = localStorage.getItem('loginTime')
+  let tasksTodo = localStorage.getItem('tasksTodo') 
+  let tasksFinished = localStorage.getItem('tasksFinished')
+  let keepTimes = localStorage.getItem('keepTimes')
+
+  tasksTodo = tasksTodo ? JSON.parse(tasksTodo) : []
+  tasksFinished = tasksFinished ? JSON.parse(tasksFinished) : []
+  keepTimes = keepTimes ? keepTimes : 0
+
+  //判断上次登录时间，如果是今天，则任务，否则清空
+  if(!loginTime){
+    loginTime = nowTime
+  }else{
+    const loginD = new Date(loginTime)
+    if(date.getDate() !== loginD.getDate() || nowTime - loginTime >= 24 * 3600 * 1000){
+      tasksFinished  = []
+      tasksTodo = []
+      localStorage.setItem('tasksFinished', JSON.stringify(tasksFinished))
+      localStorage.setItem('tasksTodo', JSON.stringify(tasksTodo))
+    }
+  }
+
+
+
+  //初始化
+  genTodo()
+  genFinished()
+
+  //绑定事件
+  tabManage.forEach((el,index) => {
+    el.addEventListener('click', () => {
+      activeTab (index)
+      activeContent (index)
+    })
+  })
+
+  taskTodo.addEventListener('click', (event) => {
+    const target = event.target
+    const index = target.getAttribute("data-index")
+    if(target.classList.contains('finish')){
+      keepTimes = +keepTimes + 1
+      tasksFinished.push(tasksTodo[index])
+      tasksTodo.splice(index,1)
+      localStorage.setItem('tasksTodo', JSON.stringify(tasksTodo))
+      localStorage.setItem('tasksFinished', JSON.stringify(tasksFinished))
+      localStorage.setItem('keepTimes', keepTimes)
+      genFinished ()
+      genTodo ()
+      activeTab (1)
+      activeContent (1)
+    }
+    else if(target.classList.contains('delete')){
+      tasksTodo.splice(index,1)
+      localStorage.setItem('tasksTodo', JSON.stringify(tasksTodo))
+      activeTab (0)
+      activeContent (0)
+      genTodo ()
+    }
+  })
+
+  closeDom.addEventListener('click', () => {
+    ipcRenderer.send('mainWindow:close')
+  })
+
+  //新建任务
+  const taskName = document.querySelector('#taskName')
+  const taskTime = document.querySelector('#taskTime')
+  document.querySelector('.submit-task').addEventListener('click',()=>{
+    const name = taskName.value, time = taskTime.value
+    tasksTodo.push({
+      name: name,
+      time: time
+    })
+    localStorage.setItem('tasksTodo', JSON.stringify(tasksTodo))
+    if(!!time) ipcRenderer.send('setTaskTimer', time, encodeURIComponent(name))
+    genTodo ()
+    activeTab (0)
+    activeContent (0)
+  })
+
+  
+  function genTodo () {
+    let todoHtml = ''
+    tasksTodo.forEach((item,index) => {
+      todoHtml +=
+      `<li class="task-item">
+        <span class="task-text">${item.name}&nbsp;&nbsp;&nbsp; ${item.time} </span>
+        <span>
+          <span class="btns finish enable-click" data-index="${index}">完成</span>
+          <span class="btns delete enable-click" data-index="${index}">删除</span>
+        </span>
+      </li>`
+    })
+    taskTodo.innerHTML = todoHtml
+  }
+
+  function genFinished () {
+    let finishHtml = ''
+    tasksFinished.forEach((item) => {
+      finishHtml += 
+      `<li class="task-item">
+        <span class="task-text">${item.name}</span>
+        <span class="flag-icon"></span>
+      </li>`
+    })
+    taskFinished.innerHTML = finishHtml
+    keepTimesDom.innerHTML = keepTimes
+  }
+
+  function activeTab (index) {
+    tabManage.forEach((tabEl) => {
+      tabEl.classList.remove('nav-active')
+    })
+    tabManage[index].classList.add('nav-active')
+  }
+
+  function activeContent (index) {
+    contentManage.forEach((taskEl) => {
+      taskEl.classList.remove('content-active')
+    })
+    contentManage[index].classList.add('content-active')
+    taskName.value = ''
+    taskTime.value = ''
+  }
+
+})();
